cdb5e0dd963a15b5e2ad59d18a8babb7478ec270
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-22 17:51:44 +0300

    added project SumOfDigits

---
+++ SumOfDigits/app.js
@@ -0,0 +1,14 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        sumOfDigits(line);
+    }
+});
+
+function sumOfDigits(line) {
+    var summ = 0;
+    line.trim().split("").forEach(function (d) {
+        summ += parseInt(d);
+    });
+    console.log(summ);
+}
