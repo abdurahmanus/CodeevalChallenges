af1214238c66352678211f6a1174f51059cb6792
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-22 17:47:11 +0300

    added project SummOfIntegersFromFile

---
+++ SumOfIntegersFromFile/app.js
@@ -0,0 +1,8 @@
+﻿var fs = require("fs"),
+    summ = 0;
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        summ += parseInt(line.trim());
+    }
+});
+console.log(summ);
