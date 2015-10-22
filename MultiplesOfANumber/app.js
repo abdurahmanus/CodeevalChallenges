ca2334c4790b3016264aad3e6d3f81d48fad0663
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-22 17:49:54 +0300

    added project MultiplesOfANumber

---
+++ MultiplesOfANumber/app.js
@@ -0,0 +1,17 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        multiplesOfANumber(line);
+    }
+});
+
+function multiplesOfANumber(line) {
+    var temp = line.trim().split(","),
+        x = parseInt(temp[0]),
+        n = parseInt(temp[1]),
+        m = 0;
+    while (m < x) {
+        m += n;
+    }
+    console.log(m);
+}
