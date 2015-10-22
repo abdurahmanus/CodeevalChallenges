f04de0ffe3951b544b2155b59eef9c69744653e6
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-22 17:37:52 +0300

    SplitTheNumber project edit

--- SplitTheNumber/app.js
+++ SplitTheNumber/app.js
@@ -1,1 +1,17 @@
-﻿console.log('Hello world');
+﻿var fs = require("fs");
+fs.readFileSync(process.argv[2]).toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        splitTheNumber(line);
+    }
+});
+
+function splitTheNumber(line) {
+    var temp = line.trim().split(" "),
+        opIndex = temp[1].indexOf("+");
+    if (opIndex !== -1) {
+        console.log(Number(temp[0].substr(0, opIndex)) + Number(temp[0].substr(opIndex)));
+    } else {
+        opIndex = temp[1].indexOf("-");
+        console.log(Number(temp[0].substr(0, opIndex)) - Number(temp[0].substr(opIndex)));
+    }
+}
