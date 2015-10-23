efd0abea3df1199222b95e7a775c878967cd29e9
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 16:10:09 +0300

    added project MultiplicationTables

---
+++ MultiplicationTables/app.js
@@ -0,0 +1,10 @@
+﻿(function() {
+    var r;
+    for (var i = 1; i <= 12; i++) {
+        r = [];
+        for (var j = 1; j <= 12; j++) {
+            r.push(("    " + i * j).slice(-4));
+        }
+        console.log(r.join("").trim());
+    }
+})();
