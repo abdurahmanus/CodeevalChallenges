335bd20e16aadf09f48b5a42bb09a25d317cd795
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 11:35:07 +0300

    added project SimpleSorting

---
+++ SimpleSorting/app.js
@@ -0,0 +1,11 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        simpleSorting(line);
+    }
+});
+
+function simpleSorting(line) {
+    var numbers = line.trim().split(" ").map(function (n) { return parseFloat(n); });
+    console.log(numbers.sort(function (a, b) { return a - b; }).map(function (n) { return n.toFixed(3); }).join(" "));
+}
