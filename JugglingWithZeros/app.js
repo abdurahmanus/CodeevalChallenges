f0859e4ed4764970433eaffc672994eb915d1bd2
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 12:44:13 +0300

    added project JugglingWithZeros

---
+++ JugglingWithZeros/app.js
@@ -0,0 +1,19 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        jugglingWithZeros(line);
+    }
+});
+
+function jugglingWithZeros(line) {
+    var temp = line.trim().split(" "),
+        binaryValue = [];
+    for (var i = 0, count = temp.length; i < count; i += 2) {
+        if (temp[i].length === 1) {
+            binaryValue.push(temp[i + 1]);
+        } else {
+            binaryValue.push(Array(temp[i + 1].length + 1).join("1"));
+        }
+    }
+    console.log(parseInt(binaryValue.join(""), 2));
+}
