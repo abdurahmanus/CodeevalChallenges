5005dd35bc82c95ac7db92ef41d47962f728b6fc
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 16:21:42 +0300

    added project RightmostChar

---
+++ RightmostChar/app.js
@@ -0,0 +1,13 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        rightmostChar(line);
+    }
+});
+
+function rightmostChar(line) {
+    var temp = line.trim().split(","),
+        s = temp[0],
+        c = temp[1];
+    console.log(s.lastIndexOf(c));
+}
