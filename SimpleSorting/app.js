7fae5631bd7a912196ed1ec6153d01818aabf6bf
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 11:38:02 +0300

    simplify simple sorting equations

--- SimpleSorting/app.js
+++ SimpleSorting/app.js
@@ -6,6 +6,5 @@ fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
 });
 
 function simpleSorting(line) {
-    var numbers = line.trim().split(" ").map(function (n) { return parseFloat(n); });
-    console.log(numbers.sort(function (a, b) { return a - b; }).map(function (n) { return n.toFixed(3); }).join(" "));
+    console.log(line.trim().split(" ").sort(function(a, b) { return parseFloat(a) - parseFloat(b) }).join(" "));
 }
