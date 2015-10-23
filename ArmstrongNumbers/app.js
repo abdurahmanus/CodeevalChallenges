4b43a26b1447926b228708fe9b351f8386131fa0
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 16:45:26 +0300

    added project ArmstrongNumbers

---
+++ ArmstrongNumbers/app.js
@@ -0,0 +1,20 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        armstrongNumbers(line);
+    }
+});
+
+function armstrongNumbers(line) {
+    var temp = line.trim(),
+        n = temp.length,
+        number = parseInt(temp),
+        digits = temp.split(""),
+        summ = 0;
+
+    digits.forEach(function(d) {
+        summ += Math.pow(parseInt(d), n);
+    });
+
+    console.log(number === summ ? "True" : "False");
+}
