9c5444f3a33d0eff355de803d3d1d6b67433ad67
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 15:48:54 +0300

    added project BitPositions

---
+++ BitPositions/app.js
@@ -0,0 +1,14 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        bitPositions(line);
+    }
+});
+
+function bitPositions(line) {
+    var temp = line.trim().split(","),
+        binaryVal = parseInt(temp[0]).toString(2),
+        p1 = binaryVal.length - parseInt(temp[1]),
+        p2 = binaryVal.length - parseInt(temp[2]);
+    console.log(binaryVal[p1] === binaryVal[p2]);
+}
