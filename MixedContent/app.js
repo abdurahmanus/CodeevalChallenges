a2936fdfed35052eb641ac45be1b85b51fe97729
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 13:03:46 +0300

    added project MixedContent

---
+++ MixedContent/app.js
@@ -0,0 +1,13 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        mixedContent(line);
+    }
+});
+
+function mixedContent(line) {
+    var temp = line.trim(),
+        digits = temp.match(/(\d)+/g),
+        words = temp.match(/([a-zA-z])+/g);
+    console.log([words ? words.join(",") : "", words && digits ? "|" : "", digits ? digits.join(",") : ""].join(""));
+}
