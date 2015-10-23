1b615377bc55bcfa074e1b48729016c50f410b28
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 11:17:17 +0300

    added project CompressedSequence

---
+++ CompressedSequence/app.js
@@ -0,0 +1,23 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        compressedSequence(line);
+    }
+});
+
+function compressedSequence(line) {
+    var numbers = line.trim().split(" "),
+        prev = NaN,
+        numCount = 0,
+        result = [];
+    for (var i = 0, count = numbers.length; i < count; i++) {
+        if (!isNaN(prev) && prev !== numbers[i]) {
+            result.push(numCount + " " + prev);
+            numCount = 0;
+        }
+        numCount++;
+        prev = numbers[i];
+    }
+    result.push(numCount + " " + prev);
+    console.log(result.join(" "));
+}
