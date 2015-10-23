3680134e34f3966d6840ed4ddba43c6eb1422ae1
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 13:08:25 +0300

    added project EvenNumbers

---
+++ EvenNumbers/app.js
@@ -0,0 +1,10 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        evenNumbers(line);
+    }
+});
+
+function evenNumbers(line) {
+    console.log(parseInt(line.trim(), 10) % 2 === 0 ? "1" : "0");
+}
