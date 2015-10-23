913e211ad5e93c73d33db1e6fab0f1f0827fba62
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 16:31:30 +0300

    added project NModM

---
+++ NModM/app.js
@@ -0,0 +1,18 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        nModM(line);
+    }
+});
+
+function nModM(line) {
+    var temp = line.trim().split(","),
+        n = parseInt(temp[0]),
+        m = parseInt(temp[1]),
+        v = 0;
+    
+    while (v <= n - m) {
+        v += m;
+    }
+    console.log(n - v);
+}
