878ff73d1359260a30f110a44760e730cc1ff367
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-23 10:46:19 +0300

    added project OneZeroTwoZeros

---
+++ OneZeroTwoZeros/app.js
@@ -0,0 +1,24 @@
+﻿var fs  = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        oneZeroTwoZeros(line);
+    }
+});
+
+function oneZeroTwoZeros(line) {
+    var temp = line.trim().split(" "),
+        numZeros = parseInt(temp[0]),
+        upperBound = parseInt(temp[1]),
+        count = 0;
+    for (var i = 1; i <= upperBound; i++) {
+        if (countZeros(i.toString(2)) === numZeros) {
+            count++;
+        }
+    }
+    console.log(count);
+}
+
+function countZeros(binaryString) {
+    var matchResult = binaryString.match(/0/g);
+    return matchResult ? matchResult.length : 0;
+}
