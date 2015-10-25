a6b02875b34f1cc8e9391ec6a99cb803fff25abc
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-25 17:00:52 +0300

    added project MultiplyLists

---
+++ MultiplyLists/app.js
@@ -0,0 +1,16 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        multiplyLists(line);
+    }
+});
+
+function multiplyLists(line) {
+    var temp = line.trim().split(" | "),
+        first = temp[0].split(" "),
+        second = temp[1].split(" "),
+        result = first.map(function (f, i) {
+            return parseInt(f) * parseInt(second[i]);
+        });
+    console.log(result.join(" "));
+}
