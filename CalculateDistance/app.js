ee2fdd570a824180bb929f79349bda4178f4a7a4
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-25 16:32:46 +0300

    added project CalculateDistance

---
+++ CalculateDistance/app.js
@@ -0,0 +1,11 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        calculateDistance(line);
+    }
+});
+
+function calculateDistance(line) {
+    var temp = line.trim().replace(/[\(\)]/g, "").split(" ").map(function (n) { return parseInt(n); });
+    console.log(Math.sqrt(Math.pow(Math.abs(temp[2] - temp[0]), 2) + Math.pow(Math.abs(temp[3] - temp[1]), 2)));
+}
