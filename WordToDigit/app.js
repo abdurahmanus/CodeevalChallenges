dab3b27f7a758afc417451b23b9d51dfcd02b046
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-25 16:50:17 +0300

    added project WordToDigit

---
+++ WordToDigit/app.js
@@ -0,0 +1,24 @@
+﻿var digits = {
+    zero: 0,
+    one: 1,
+    two: 2,
+    three: 3,
+    four: 4,
+    five: 5,
+    six: 6,
+    seven: 7,
+    eight: 8,
+    nine: 9
+};
+
+var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        wordToDigit(line);
+    }
+});
+
+function wordToDigit(line) {
+    var result = line.trim().split(";").map(function (w) { return digits[w]; });
+    console.log(result.join(""));
+}
