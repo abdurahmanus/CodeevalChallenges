c06b94da1f2a65f466291915e06492ab1ed68716
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-25 16:41:09 +0300

    added project PenultimateWord

---
+++ PenultimateWord/app.js
@@ -0,0 +1,11 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        penultimateWord(line);
+    }
+});
+
+function penultimateWord(line) {
+    var words = line.trim().split(" ");
+    console.log(words[words.length - 2]);
+}
