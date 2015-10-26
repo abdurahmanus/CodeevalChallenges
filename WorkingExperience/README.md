ea6db85526efc27d18d103a5403e901457572c15
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-26 17:40:01 +0300

    added project WorkingExperience

---
+++ WorkingExperience/README.md
@@ -0,0 +1,3 @@
+﻿# WorkingExperience
+
+
---
+++ WorkingExperience/app.js
@@ -0,0 +1,14 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        workingExperience(line);
+    }
+});
+
+function workingExperience(line) {
+    var periods = line.trim().split("; ").map(function(p) {
+        var temp = p.split("-");
+        return { from: Date.parse(temp[0]), to: Date.parse(temp[1]) };
+    });
+    console.log(periods);
+}
