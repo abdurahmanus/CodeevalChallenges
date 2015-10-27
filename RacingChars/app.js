34dca8eca6b88aa90c26b5db43828107c7869c85
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-27 16:40:18 +0300

    added project RacingChars

---
+++ RacingChars/app.js
@@ -0,0 +1,29 @@
+﻿var prevPassIndex;
+var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        racingChars(line);
+    }
+});
+
+function racingChars(line) {
+    var section = line.trim().split(""),
+        passIndex = section.indexOf("C");
+    
+    if (passIndex === -1)
+        passIndex = section.indexOf("_");
+
+    section[passIndex] = movementChar(prevPassIndex, passIndex);
+    prevPassIndex = passIndex;
+    console.log(section.join(""));
+}
+
+function movementChar(prevPassIndex, passIndex) {
+    if (prevPassIndex === undefined || prevPassIndex === passIndex) {
+        return "|";
+    } else if (prevPassIndex < passIndex) {
+        return "\\";
+    } else {
+        return "/";
+    }
+}
