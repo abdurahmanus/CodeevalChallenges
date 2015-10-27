b0294242208b4e2ccda7764300c206a26582a638
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-27 12:38:29 +0300

    added project RoadTrip

---
+++ RoadTrip/app.js
@@ -0,0 +1,20 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        roadTrip(line);
+    }
+});
+
+function roadTrip(line) {
+    var min = 0;
+    var result = line.trim().match(/(\d)+/g).map(function(n) {
+         return parseInt(n);
+    }).sort(function(a, b) {
+         return a - b;
+    }).map(function (v) {
+        var r = v - min;
+        min = v;
+        return r;
+    });
+    console.log(result.join(","));
+}
