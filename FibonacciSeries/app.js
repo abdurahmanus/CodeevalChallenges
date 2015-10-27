7340d9177d2358dd91ad3fd5a5cd7e842a5db556
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-27 15:08:17 +0300

    FibonacciSeries and StringsAndArrows

---
+++ FibonacciSeries/app.js
@@ -0,0 +1,18 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        fibonacciSeries(line);
+    }
+});
+
+function fibonacciSeries(line) {
+    console.log(fibonacci(parseInt(line.trim())));
+}
+
+function fibonacci(n) {
+    if (n > 1) {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    } else {
+        return n;
+    }
+}
