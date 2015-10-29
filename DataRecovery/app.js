5df64ea8118188cdb4a6f5abe1e34a88cd0589fc
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-29 10:35:26 +0300

    added project DataRecovery

---
+++ DataRecovery/app.js
@@ -0,0 +1,18 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        dataRecovery(line);
+    }
+});
+
+function dataRecovery(line) {
+    var temp = line.trim().split(";"),
+        words = temp[0].split(" "),
+        numbers = temp[1].split(" ").map(function(n) { return parseInt(n); }),
+        result = Array.apply(null, Array(words.length));
+    numbers.forEach(function(n, i) {
+        result[n - 1] = words[i];
+    });
+    result[result.indexOf(undefined)] = words[words.length - 1];
+    console.log(result.join(" "));
+}
