480e667001fd97a5a5ce057757870f2461de707c
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-10-29 09:27:34 +0300

    added project MinimumDistance

---
+++ MinimumDistance/app.js
@@ -0,0 +1,23 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        minimumDistance(line);
+    }
+});
+
+function minimumDistance(line) {
+    var numbers = line.trim().split(" ").map(function (n) { return parseInt(n); }),
+        addresses = numbers.slice(1),
+        min = Number.MAX_VALUE;
+    addresses.forEach(function(a) {
+        var summ = calcSumOfDistances(a, addresses);
+        if (summ < min) min = summ;
+    });
+    console.log(min);
+}
+
+function calcSumOfDistances(adr, addresses) {
+    return addresses.reduce(function (sum, current) {
+        return sum + Math.abs(adr - current);
+    }, 0);
+}
