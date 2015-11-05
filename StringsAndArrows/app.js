6e52ddf48c018e780a4ad0a367980809d3152a85
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-11-05 12:48:33 +0300

    solved StringsAndArrows

--- StringsAndArrows/app.js
+++ StringsAndArrows/app.js
@@ -6,5 +6,13 @@ fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
 });
 
 function stringsAndArrows(line) {
-    
+    var temp = line.trim(),
+        total = 0;
+    for (var i = 0, count = temp.length - 4; i < count; i++) {
+        var s = temp.substr(i, 5);
+        if (s === ">>-->" || s === "<--<<") {
+            total++;
+        }
+    }
+    console.log(total);
 }
