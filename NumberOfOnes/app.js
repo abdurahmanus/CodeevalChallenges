138e36b077375dcbf904ebc88cbcfcf2e46059ba
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-11-05 17:06:01 +0300

    added project NumberOfOnes

---
+++ NumberOfOnes/app.js
@@ -0,0 +1,12 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        numberOfOnes(line);
+    }
+});
+
+function numberOfOnes(line) {
+    var bits = parseInt(line.trim()).toString(2),
+        matchResult = bits.match(/1/g);
+    console.log(matchResult ? matchResult.length : 0);
+}
