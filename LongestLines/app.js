b17686716434811933786db39d64a39157174c71
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-11-05 14:20:46 +0300

    added project LongestLines

---
+++ LongestLines/app.js
@@ -0,0 +1,16 @@
+﻿var fs = require("fs"),
+    temp = [],
+    numberOfOutput;
+
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        temp.push(line.trim());
+    }
+});
+
+numberOfOutput = parseInt(temp[0]);
+temp.slice(1).sort(function(a, b) {
+    return b.length - a.length;
+}).slice(0, numberOfOutput).forEach(function(t) {
+    console.log(t);
+});
