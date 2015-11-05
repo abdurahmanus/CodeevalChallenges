2906a893f112b450e632c6eec7ac60a0ef2fb491
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-11-05 15:51:32 +0300

    added project RemoveCharacters

---
+++ RemoveCharacters/app.js
@@ -0,0 +1,13 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        removeCharacters(line);
+    }
+});
+
+function removeCharacters(line) {
+    var temp = line.trim().split(", "),
+        pattern = new RegExp("[" + temp[1] + "]", "g"),
+        result = temp[0].replace(pattern, "");
+    console.log(result);
+}
