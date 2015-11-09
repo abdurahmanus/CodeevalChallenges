c582d925da85f3b75b1d874fbcd8681d96967c2b
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-11-09 17:28:36 +0300

    added project SumOfIntegers

---
+++ SumOfIntegers/app.js
@@ -0,0 +1,24 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        sumOfIntegers(line);
+    }
+});
+
+function sumOfIntegers(line) {
+    var arr = line.trim().split(",").map(function(i) { return parseInt(i); }),
+        temp = [],
+        count = arr.length;
+    for (var i = 0; i < count; i++) {
+        for (var j = count; j > i; j--) {
+            temp.push(summOf(arr.slice(i, j)));
+        }
+    }
+    console.log(Math.max.apply(this, temp));
+}
+
+function summOf(values) {
+    return values.reduce(function (sum, current) {
+        return sum + current;
+    }, 0);
+}
