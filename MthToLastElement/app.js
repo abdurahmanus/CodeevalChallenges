06d0cbb86b54bdcd5ee1331770dbbc390b720a6e
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-11-09 15:49:20 +0300

    added project MthToLastElement

---
+++ MthToLastElement/app.js
@@ -0,0 +1,15 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        mthToLastElement(line);
+    }
+});
+
+function mthToLastElement(line) {
+    var temp = line.trim().split(" "),
+        chars = temp.slice(0, temp.length - 1),
+        index = parseInt(temp[temp.length - 1]);
+    if (index <= chars.length) {
+        console.log(chars[chars.length - index]);
+    }
+}
