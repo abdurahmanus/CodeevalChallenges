decedc11072b17ac76dee70c1df78aa69b05554f
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-11-13 15:58:31 +0300

    added project Endianness

---
+++ Endianness/app.js
@@ -0,0 +1,6 @@
+﻿var littleEndian = (function () {
+    var buffer = new ArrayBuffer(2);
+    new DataView(buffer).setInt16(0, 256, true);
+    return new Int16Array(buffer)[0] === 256;
+})();
+console.log(littleEndian === true ? "LittleEndian" : "BigEndian");
