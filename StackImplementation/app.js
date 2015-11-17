5aae31aee04369333880b3672691348d4c727c69
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-11-17 22:56:01 +0300

    added project StackImplementation

---
+++ StackImplementation/app.js
@@ -0,0 +1,22 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        stackImplementation(line);
+    }
+});
+
+function stackImplementation(line) {
+    var temp = line.trim().split(" "),
+        stack = [];
+    temp.forEach(function(i) {
+        stack.push(i);
+    });
+    var alternate = true,
+        popped,
+        result = [];
+    while ((popped = stack.pop())) {
+        if (alternate === true) result.push(popped);
+        alternate = !alternate;
+    }
+    console.log(result.join(" "));
+}
