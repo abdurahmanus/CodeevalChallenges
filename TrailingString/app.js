0c13924a802d851f724bebda19d95fa99c145732
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-11-18 21:47:17 +0300

    added project TrailingString

---
+++ TrailingString/app.js
@@ -0,0 +1,15 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        trailingString(line);
+    }
+});
+
+function trailingString(line) {
+    var temp = line.trim().split(",");
+    console.log(endsWith(temp[0], temp[1]) ? 1 : 0);
+}
+
+function endsWith(str, suffix) {
+    return str.indexOf(suffix, str.length - suffix.length) !== -1;
+}
