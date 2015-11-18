7cf108f86960aeccab983f5b8813c1510c5e08fa
Dmitry Gladkikh <abdurahmanus@gmail.com>
2015-11-18 21:33:12 +0300

    added project DecimalToBinary

---
+++ DecimalToBinary/README.md
@@ -0,0 +1,3 @@
+﻿# DecimalToBinary
+
+
---
+++ DecimalToBinary/app.js
@@ -0,0 +1,10 @@
+﻿var fs = require("fs");
+fs.readFileSync("input.txt").toString().split("\n").forEach(function (line) {
+    if (line !== "") {
+        decimalToBinary(line);
+    }
+});
+
+function decimalToBinary(line) {
+    console.log(parseInt(line.trim()).toString(2));
+}
